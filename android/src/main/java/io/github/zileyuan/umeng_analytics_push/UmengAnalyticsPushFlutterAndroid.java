7be4c1037d34047296cccf330922eb10d3f65ece
cai <caily5@163.com>
2022-11-21 11:38:16 +0800

    fix

--- android/src/main/java/io/github/zileyuan/umeng_analytics_push/UmengAnalyticsPushFlutterAndroid.java
+++ android/src/main/java/io/github/zileyuan/umeng_analytics_push/UmengAnalyticsPushFlutterAndroid.java
@@ -1,23 +1,23 @@
 package io.github.zileyuan.umeng_analytics_push;
 
 import android.app.Application;
-import android.app.Activity;
-import android.util.Log;
 import android.content.Context;
 import android.content.Intent;
-import com.umeng.commonsdk.UMConfigure;
-import com.umeng.message.IUmengRegisterCallback;
+import android.util.Log;
+
 import com.umeng.analytics.MobclickAgent;
-import com.umeng.message.PushAgent;
+import com.umeng.commonsdk.UMConfigure;
 import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
 import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.api.UPushRegisterCallback;
 import com.umeng.message.entity.UMessage;
 
+import org.android.agoo.honor.HonorRegister;
 import org.android.agoo.huawei.HuaWeiRegister;
 import org.android.agoo.mezu.MeizuRegister;
 import org.android.agoo.oppo.OppoRegister;
 import org.android.agoo.vivo.VivoRegister;
-import org.android.agoo.honor.HonorRegister;
 
 public class UmengAnalyticsPushFlutterAndroid {
 
@@ -44,38 +44,39 @@ public class UmengAnalyticsPushFlutterAndroid {
         String messageSecret = UmengAnalyticsPushFlutterAndroid.messageSecret;
         UMConfigure.setLogEnabled(logEnabled);
         UMConfigure.init(context, appkey, channel, UMConfigure.DEVICE_TYPE_PHONE, messageSecret);
-        if (pushEnabled && messageSecret != null && !messageSecret.isEmpty()) {
-            //获取消息推送代理示例
-            PushAgent mPushAgent = PushAgent.getInstance(context);
-            mPushAgent.setResourcePackageName(BuildConfig.LIBRARY_PACKAGE_NAME);
-            //设置客户端允许声音提醒
-            mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
-            //设置客户端允许呼吸灯点亮
-            mPushAgent.setNotificationPlayLights(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
-            //设置客户端允许震动
-            mPushAgent.setNotificationPlayVibrate(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
-            //设置冷却时间，避免三分钟内出现多条通知而被替换
-            mPushAgent.setMuteDurationSeconds(180);
-            //注册推送服务，每次调用register方法都会回调该接口
-            mPushAgent.register(new IUmengRegisterCallback() {
-                @Override
-                public void onSuccess(String deviceToken) {
-                    //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
-                    Log.i("umeng_push_register", "注册成功：deviceToken：-------->  " + deviceToken);
 
-                }
-                @Override
-                public void onFailure(String s, String s1) {
-                    Log.e("umeng_push_register", "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
-                }
-            });
-            UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
-                @Override
-                public void handleMessage(Context context, UMessage msg) {
-                    super.handleMessage(context, msg);
+        //获取消息推送代理示例
+        PushAgent mPushAgent = PushAgent.getInstance(context);
+        mPushAgent.setResourcePackageName("com.ly_sky.ly_control");
+        //设置客户端允许声音提醒
+        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+        //设置客户端允许呼吸灯点亮
+        mPushAgent.setNotificationPlayLights(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+        //设置客户端允许震动
+        mPushAgent.setNotificationPlayVibrate(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+        //设置冷却时间，避免三分钟内出现多条通知而被替换
+        mPushAgent.setMuteDurationSeconds(180);
+        //注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new UPushRegisterCallback() {
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
+                Log.i("umeng_push_register", "注册成功：deviceToken：-------->  " + deviceToken);
+
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.e("umeng_push_register", "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+            }
+        });
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
+            @Override
+            public void handleMessage(Context context, UMessage msg) {
+                super.handleMessage(context, msg);
 //                    rouseMainActivity(context);
-                    UmengAnalyticsPushPlugin.eventSink.success(msg.getRaw().toString());
-                }
+                UmengAnalyticsPushPlugin.eventSink.success(msg.getRaw().toString());
+            }
 //                    @Override
 //                    public void dealWithCustomAction(Context context, UMessage msg) {
 //                        Log.i("dealWithCustomAction --------->  ", msg.custom);
@@ -83,12 +84,12 @@ public class UmengAnalyticsPushFlutterAndroid {
 //                        rouseMainActivity(context);
 ////                        UmengAnalyticsPushPlugin.eventSink.success(msg.getRaw().toString());
 //                    }
-            };
-            mPushAgent.setNotificationClickHandler(notificationClickHandler);
-            //后台进行日活统计及多维度推送的必调用方法，请务必调用
-            mPushAgent.onAppStart();
-            UmengAnalyticsPushFlutterAndroid.UmengPushAgent = mPushAgent;
-        }
+        };
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+        //后台进行日活统计及多维度推送的必调用方法，请务必调用
+        mPushAgent.onAppStart();
+        UmengAnalyticsPushFlutterAndroid.UmengPushAgent = mPushAgent;
+
         //选择AUTO自动采集模式（必须！）
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
     }
--- android/src/main/java/io/github/zileyuan/umeng_analytics_push/UmengAnalyticsPushPlugin.java
+++ android/src/main/java/io/github/zileyuan/umeng_analytics_push/UmengAnalyticsPushPlugin.java
@@ -1,214 +1,214 @@
 package io.github.zileyuan.umeng_analytics_push;
 
+import android.content.Context;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
-import android.content.Context;
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.message.api.UPushAliasCallback;
+import com.umeng.message.api.UPushTagCallback;
+import com.umeng.message.common.inter.ITagManager;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-import com.umeng.message.IUmengRegisterCallback;
-import com.umeng.message.MsgConstant;
-import com.umeng.message.PushAgent;
-import com.umeng.message.UTrack;
-import com.umeng.message.UmengNotificationClickHandler;
-import com.umeng.message.entity.UMessage;
-import com.umeng.message.tag.TagManager;
-import com.umeng.message.common.inter.ITagManager;
-import com.umeng.analytics.MobclickAgent;
-import android.util.Log;
-
-/** UmengAnalyticsPushPlugin */
+/**
+ * UmengAnalyticsPushPlugin
+ */
 public class UmengAnalyticsPushPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  public static MethodChannel methodChannel;
-  public static EventChannel eventChannel;
-  public static EventChannel.EventSink eventSink;
-  private static Context context;
-  public static String deviceToken;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    public static MethodChannel methodChannel;
+    public static EventChannel eventChannel;
+    public static EventChannel.EventSink eventSink;
+    private static Context context;
+    public static String deviceToken;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    context = flutterPluginBinding.getApplicationContext();
-    methodChannel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "umeng_analytics_push");
-    methodChannel.setMethodCallHandler(this);
-    eventChannel = new EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "umeng_analytics_push/stream");
-    eventChannel.setStreamHandler(
-      new EventChannel.StreamHandler() {
-        @Override
-        public void onListen(Object args, final EventChannel.EventSink events) {
-            Log.i("eventChannel", "adding listener");
-            eventSink = events;
-        }
-        @Override
-        public void onCancel(Object args) {
-            Log.i("eventChannel", "cancelling listener");
-            eventSink = null;
-        }
-      }
-    );
-  }
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        context = flutterPluginBinding.getApplicationContext();
+        methodChannel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "umeng_analytics_push");
+        methodChannel.setMethodCallHandler(this);
+        eventChannel = new EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "umeng_analytics_push/stream");
+        eventChannel.setStreamHandler(
+                new EventChannel.StreamHandler() {
+                    @Override
+                    public void onListen(Object args, final EventChannel.EventSink events) {
+                        Log.i("eventChannel", "adding listener");
+                        eventSink = events;
+                    }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    methodChannel.setMethodCallHandler(null);
-    eventChannel.setStreamHandler(null);
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    context = registrar.context();
-    methodChannel = new MethodChannel(registrar.messenger(), "umeng_analytics_push");
-    methodChannel.setMethodCallHandler(new UmengAnalyticsPushPlugin());
-    eventChannel = new EventChannel(registrar.messenger(), "umeng_analytics_push/stream");
-    eventChannel.setStreamHandler(
-      new EventChannel.StreamHandler() {
-        @Override
-        public void onListen(Object args, final EventChannel.EventSink events) {
-            Log.i("eventChannel", "adding listener");
-            eventSink = events;
-        }
-        @Override
-        public void onCancel(Object args) {
-            Log.i("eventChannel", "cancelling listener");
-            eventSink = null;
-        }
-      }
-    );
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-
-    if (call.method.equals("initUmeng")) {
-      initUmeng(call, result);
-    } else if (call.method.equals("addTags")) {
-      addTags(call, result);
-    } else if (call.method.equals("deleteTags")) {
-      deleteTags(call, result);
-    } else if (call.method.equals("addAlias")) {
-      addAlias(call, result);
-    } else if (call.method.equals("setAlias")) {
-      setAlias(call, result);
-    } else if (call.method.equals("deleteAlias")) {
-      deleteAlias(call, result);
-    } else if (call.method.equals("pageStart")) {
-      pageStart(call, result);
-    } else if (call.method.equals("pageEnd")) {
-      pageEnd(call, result);
-    } else if (call.method.equals("event")) {
-      event(call, result);
-    } else if (call.method.equals("deviceToken")) {
-      deviceToken(call, result);
-    } else {
-      result.notImplemented();
+                    @Override
+                    public void onCancel(Object args) {
+                        Log.i("eventChannel", "cancelling listener");
+                        eventSink = null;
+                    }
+                }
+        );
     }
-  }
 
-  private void event(MethodCall call, Result result) {
-    String eventId = call.argument("eventId");
-    String label = call.argument("label");
-    MobclickAgent.onEvent(context, eventId, label);
-  }
-
-  private void pageStart(MethodCall call, Result result) {
-    String pageName = call.argument("pageName");
-    MobclickAgent.onPageStart(pageName);
-  }
-
-  private void pageEnd(MethodCall call, Result result) {
-    String pageName = call.argument("pageName");
-    MobclickAgent.onPageEnd(pageName);
-  }
-
-  private void addTags(MethodCall call, Result result) {
-    String tags = call.argument("tags");
-    if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
-      UmengAnalyticsPushFlutterAndroid.UmengPushAgent.getTagManager().addTags(new TagManager.TCallBack() {
-        @Override
-        public void onMessage(final boolean isSuccess, final ITagManager.Result result) {
-          Log.i("umeng_push_tags", "addTags：--> " + String.valueOf(isSuccess));
-        }
-      }, tags);
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+        eventChannel.setStreamHandler(null);
     }
-  }
 
-  private void deleteTags(MethodCall call, Result result) {
-    String tags = call.argument("tags");
-    if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
-      UmengAnalyticsPushFlutterAndroid.UmengPushAgent.getTagManager().deleteTags(new TagManager.TCallBack() {
-        @Override
-        public void onMessage(final boolean isSuccess, final ITagManager.Result result) {
-          Log.i("umeng_push_tags", "deleteTags：--> " + String.valueOf(isSuccess));
-        }
-      }, tags);
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+    // plugin registration via this function while apps migrate to use the new Android APIs
+    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+    //
+    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+    // in the same class.
+    public static void registerWith(Registrar registrar) {
+        context = registrar.context();
+        methodChannel = new MethodChannel(registrar.messenger(), "umeng_analytics_push");
+        methodChannel.setMethodCallHandler(new UmengAnalyticsPushPlugin());
+        eventChannel = new EventChannel(registrar.messenger(), "umeng_analytics_push/stream");
+        eventChannel.setStreamHandler(
+                new EventChannel.StreamHandler() {
+                    @Override
+                    public void onListen(Object args, final EventChannel.EventSink events) {
+                        Log.i("eventChannel", "adding listener");
+                        eventSink = events;
+                    }
+
+                    @Override
+                    public void onCancel(Object args) {
+                        Log.i("eventChannel", "cancelling listener");
+                        eventSink = null;
+                    }
+                }
+        );
     }
-  }
 
-  private void addAlias(MethodCall call, Result result) {
-    String alias = call.argument("alias");
-    String type = call.argument("type");
-    if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
-      UmengAnalyticsPushFlutterAndroid.UmengPushAgent.addAlias(alias, type, new UTrack.ICallBack() {
-        @Override
-        public void onMessage(boolean isSuccess, String message) {
-          Log.i("umeng_push_alias", "addAlias：--> " + String.valueOf(isSuccess) + "; 消息：--> " + message);
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+
+        if (call.method.equals("initUmeng")) {
+            initUmeng(call, result);
+        } else if (call.method.equals("addTags")) {
+            addTags(call, result);
+        } else if (call.method.equals("deleteTags")) {
+            deleteTags(call, result);
+        } else if (call.method.equals("addAlias")) {
+            addAlias(call, result);
+        } else if (call.method.equals("setAlias")) {
+            setAlias(call, result);
+        } else if (call.method.equals("deleteAlias")) {
+            deleteAlias(call, result);
+        } else if (call.method.equals("pageStart")) {
+            pageStart(call, result);
+        } else if (call.method.equals("pageEnd")) {
+            pageEnd(call, result);
+        } else if (call.method.equals("event")) {
+            event(call, result);
+        } else if (call.method.equals("deviceToken")) {
+            deviceToken(call, result);
+        } else {
+            result.notImplemented();
         }
-      });
     }
-  }
 
-  private void setAlias(MethodCall call, Result result) {
-    String alias = call.argument("alias");
-    String type = call.argument("type");
-    if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
-      UmengAnalyticsPushFlutterAndroid.UmengPushAgent.setAlias(alias, type, new UTrack.ICallBack() {
-        @Override
-        public void onMessage(boolean isSuccess, String message) {
-          Log.i("umeng_push_alias", "setAlias：--> " + String.valueOf(isSuccess) + "; 消息：--> " + message);
+    private void event(MethodCall call, Result result) {
+        String eventId = call.argument("eventId");
+        String label = call.argument("label");
+        MobclickAgent.onEvent(context, eventId, label);
+    }
+
+    private void pageStart(MethodCall call, Result result) {
+        String pageName = call.argument("pageName");
+        MobclickAgent.onPageStart(pageName);
+    }
+
+    private void pageEnd(MethodCall call, Result result) {
+        String pageName = call.argument("pageName");
+        MobclickAgent.onPageEnd(pageName);
+    }
+
+    private void addTags(MethodCall call, Result result) {
+        String tags = call.argument("tags");
+        if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
+            UmengAnalyticsPushFlutterAndroid.UmengPushAgent.getTagManager().addTags(new UPushTagCallback<ITagManager.Result>() {
+                @Override
+                public void onMessage(final boolean isSuccess, final ITagManager.Result result) {
+                    Log.i("umeng_push_tags", "addTags：--> " + String.valueOf(isSuccess));
+                }
+            }, tags);
         }
-      });
     }
-  }
 
-  private void deleteAlias(MethodCall call, Result result) {
-    String alias = call.argument("alias");
-    String type = call.argument("type");
-    if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
-      UmengAnalyticsPushFlutterAndroid.UmengPushAgent.deleteAlias(alias, type, new UTrack.ICallBack() {
-        @Override
-        public void onMessage(boolean isSuccess, String message) {
-          Log.i("umeng_push_alias", "deleteAlias：--> " + String.valueOf(isSuccess) + "; 消息：--> " + message);
+    private void deleteTags(MethodCall call, Result result) {
+        String tags = call.argument("tags");
+        if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
+            UmengAnalyticsPushFlutterAndroid.UmengPushAgent.getTagManager().deleteTags(new UPushTagCallback<ITagManager.Result>() {
+                @Override
+                public void onMessage(boolean isSuccess, ITagManager.Result result) {
+                    Log.i("umeng_push_tags", "deleteTags：--> " + String.valueOf(isSuccess));
+                }
+            }, tags);
         }
-      });
     }
-  }
+
+    private void addAlias(MethodCall call, Result result) {
+        String alias = call.argument("alias");
+        String type = call.argument("type");
+        if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
+            UmengAnalyticsPushFlutterAndroid.UmengPushAgent.addAlias(alias, type, new UPushAliasCallback() {
+                @Override
+                public void onMessage(boolean isSuccess, String message) {
+                    Log.i("umeng_push_alias", "addAlias：--> " + String.valueOf(isSuccess) + "; 消息：--> " + message);
+                }
+            });
+        }
+    }
+
+    private void setAlias(MethodCall call, Result result) {
+        String alias = call.argument("alias");
+        String type = call.argument("type");
+        if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
+            UmengAnalyticsPushFlutterAndroid.UmengPushAgent.setAlias(alias, type, new UPushAliasCallback() {
+                @Override
+                public void onMessage(boolean isSuccess, String message) {
+                    Log.i("umeng_push_alias", "setAlias：--> " + String.valueOf(isSuccess) + "; 消息：--> " + message);
+                }
+            });
+        }
+    }
+
+    private void deleteAlias(MethodCall call, Result result) {
+        String alias = call.argument("alias");
+        String type = call.argument("type");
+        if (UmengAnalyticsPushFlutterAndroid.UmengPushAgent != null) {
+            UmengAnalyticsPushFlutterAndroid.UmengPushAgent.deleteAlias(alias, type, new UPushAliasCallback() {
+                @Override
+                public void onMessage(boolean isSuccess, String message) {
+                    Log.i("umeng_push_alias", "deleteAlias：--> " + String.valueOf(isSuccess) + "; 消息：--> " + message);
+                }
+            });
+        }
+    }
 
 
-  private void deviceToken(MethodCall call, Result result) {
-    String deviceToken = UmengAnalyticsPushFlutterAndroid.UmengPushAgent.getRegistrationId();
-    result.success(deviceToken);
-  }
+    private void deviceToken(MethodCall call, Result result) {
+        String deviceToken = UmengAnalyticsPushFlutterAndroid.UmengPushAgent.getRegistrationId();
+        result.success(deviceToken);
+    }
 
-  private void initUmeng(MethodCall call, Result result) {
-    boolean logEnabled = call.argument("logEnabled");
-    boolean pushEnabled = call.argument("pushEnabled");
-    UmengAnalyticsPushFlutterAndroid.androidInit(logEnabled, pushEnabled);
-  }
+    private void initUmeng(MethodCall call, Result result) {
+        boolean logEnabled = call.argument("logEnabled");
+        boolean pushEnabled = call.argument("pushEnabled");
+        UmengAnalyticsPushFlutterAndroid.androidInit(logEnabled, pushEnabled);
+    }
 
 }
