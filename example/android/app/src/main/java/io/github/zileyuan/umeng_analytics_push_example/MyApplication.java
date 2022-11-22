21e17d505e0024ad3b4dd43033fe1c3f9fac04ec
cai <caily5@163.com>
2022-11-22 11:32:07 +0800

    fix

---
+++ example/android/app/src/main/java/io/github/zileyuan/umeng_analytics_push_example/MyApplication.java
@@ -0,0 +1,11 @@
+package io.github.zileyuan.umeng_analytics_push_example;
+
+import io.flutter.app.FlutterApplication;
+
+public class MyApplication extends FlutterApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
