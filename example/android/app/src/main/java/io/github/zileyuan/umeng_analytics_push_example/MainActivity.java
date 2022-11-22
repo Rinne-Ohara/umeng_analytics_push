7b9ebb712d9c41d152ad4b322b6fe776f22edbac
cai <caily5@163.com>
2022-11-22 11:51:40 +0800

    fix

--- example/android/app/src/main/java/io/github/zileyuan/umeng_analytics_push_example/MainActivity.java
+++ example/android/app/src/main/java/io/github/zileyuan/umeng_analytics_push_example/MainActivity.java
@@ -2,12 +2,10 @@ package io.github.zileyuan.umeng_analytics_push_example;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
   }
 }
